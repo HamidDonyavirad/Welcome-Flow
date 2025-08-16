09589eb1654ca9674c598e3a78d0e62688962780
hamid0 <hamid@DESKTOP-MP39UOF.localdomain>
2025-08-16 09:34:10 +0330

    feat(models.py):Creating models for the project

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+class EmailStatus(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    email_type = models.CharField(max_length=50)  
+    sent_at = models.DateTimeField(auto_now_add=True)
+    success = models.BooleanField(default=False)
+    error_message = models.TextField(blank=True, null=True)
