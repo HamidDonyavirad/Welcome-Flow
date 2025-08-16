59075306686b22d8e33773f59278b170366ba151
hamid0 <hamid@DESKTOP-MP39UOF.localdomain>
2025-08-16 21:49:34 +0330

    feat(views.py): Adding  registry class

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer
+from .tasks import schedule_welcome_emails
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+class RegisterAPIView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            schedule_welcome_emails(user.id)
+
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token)
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
