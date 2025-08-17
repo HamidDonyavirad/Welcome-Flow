11e08b45f0d0e11b4a80df813b506401a69ec2d7
hamid0 <hamid@DESKTOP-MP39UOF.localdomain>
2025-08-17 09:49:27 +0330

    fix(settings.py): Fix errors related to settings

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'accounts',
 ]
 
@@ -138,6 +139,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = ' '       # use app password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
